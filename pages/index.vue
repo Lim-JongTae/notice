7e86aa69285db5e19f7866b73fda00ea729a9ecb
JongTae <ljt0721@gmail.com>
2024-11-07 15:43:10 +0900

    debudo img

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header class="header">
+    <!-- <header class="header">
       <NuxtLink to="#" class="logo">Portfolio</NuxtLink>
       <UIcon name="i-mdi-menu" id="menu-icon"/>
       <nav class="navbar">
@@ -10,40 +10,47 @@
         <NuxtLink class="link" to="#Portfolio">Portfolio</NuxtLink>
         <NuxtLink class="link" to="#Content">Content</NuxtLink>
       </nav>
-    </header>
+    </header> -->
     <!-- home section design -->
+    <div class="services-container">
+      <div class="services-box">
+        <img src="/img2.png" class="img-edit mx-auto" />      
+      </div>
+    </div>
+    <div class="home-img">
+      
+    </div>
      <section class="home" id="home">
       <div class="home-content">
         <h3>안녕하십니까?</h3>
-        <h1>대부도탐조대회</h1>
-        <h3>탐조대회를 <span>개최하게 되었습니다.</span></h3>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa excepturi praesentium quis, ea eveniet maxime voluptatum consequatur animi repudiandae nesciunt repellendus nisi voluptatibus in deleniti aut laboriosam facilis adipisci rerum.</p>
+        <h1>2024년</h1>
+        <h3>대부도 탐조대회를 <span>개최하게 되었습니다.</span></h3>
+        <p>이동성 물새들의 계절을 맞아<span> 안산 대부도의 EAAF(동아시아-대양주 철새이동경로)지역</span>의 자연생태를 체험하고 대부도
+           서식지의 이해와 탐조관광의 기회를 확대하고자 하오니 많은 참석 바랍니다.</p>
         <div class="social-media">
           <NuxtLink class="link-a"><UIcon name="i-mdi-facebook" ></UIcon></NuxtLink>
           <NuxtLink class="link-a"><UIcon name="i-mdi-youtube" ></UIcon></NuxtLink>
           <NuxtLink class="link-a"><UIcon name="i-mdi-twitter"></UIcon></NuxtLink>
           <NuxtLink class="link-a"><UIcon name="i-mdi-instagram"></UIcon></NuxtLink>
         </div>
-        <NuxtLink to="#" class="btn">Download CV</NuxtLink>
+        <NuxtLink to="#" class="btn">참석신청</NuxtLink>
       </div>
-      <div class="home-img">
-        <img src="/img1.png" />
-      </div>
-     </section>
-     <!-- about section design -->
+    </section>
+    <!-- about section design -->
+    
       <section class="about" id="about">
         <div class="about-img">
           <img src="/img1.png" alt="">
         </div>
         <div class="about-content">
-          <h2 class="heading">대부도<span>탐조대회</span> </h2>
-          <h3>탐조대회</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ipsam ex unde numquam possimus illo sapiente natus vel sunt. Sed asperiores nostrum, voluptates dolore quis tempora dicta eveniet incidunt blanditiis?</p>
-          <NuxtLink to="#" class="btn">Read More</NuxtLink>
+          <h2 class="heading">대부도<span>탐조대회</span></h2>
+          <h3>탐조대회신청 접수</h3>
+          <p>아래의 내용을 참고 하여 신청해 주시기 바랍니다</p>
+          <!-- <NuxtLink to="#" class="btn">Read More</NuxtLink> -->
         </div>
       </section>
       <!-- services section design -->
-       <section class="services" id="services">
+       <!-- <section class="services" id="services">
         <h2 class="heading">Our <span>Services</span></h2>
         <div class="services-container">
           <div class="services-box">
@@ -65,22 +72,22 @@
             <NuxtLink to="#" class="btn">Read More</NuxtLink>
           </div>
         </div>
-       </section>
+       </section> -->
        <section class="portfolio" id="portfolio" >
-        <h2 class="heading">Latest <span>Project</span></h2>        
+        <h2 class="heading"><span>대회안내</span></h2>        
         <div class="portfolio-container">
-          <div class="portfolio-box" v-for="item in portfolio" :key="item.idx">
-            <img :src="`${ item.img }`" alt="" />
-            <div class="portfolio-layer">              
+          <div class="portfolio-box">
+            <img src="../public/dabudo.png" alt="" />
+            <!-- <div class="portfolio-layer">              
               <h4>{{item.job}}</h4>
               <p>{{ item.des }}</p>
               <NuxtLink class="link-icon" to="#"><UIcon :name="`${item.icon}`" class="w-8 h-8 text-gray-950"></UIcon></NuxtLink>
-            </div>
+            </div> -->
           </div>
         </div>        
        </section>
        <!-- contact section disign -->
-       <section class="contact" id="contact">
+       <!-- <section class="contact" id="contact">
         <h2 class="heading"> <span>Me!</span></h2>
         <form action="">
           <div class="input-box">
@@ -94,15 +101,15 @@
           <textarea name="" id="" placeholder="Your Message"></textarea>
           <input type="submit" value="Send Message" class="btn">
         </form>
-       </section>
+       </section> -->
        <!-- Footer Design -->
         <footer class="footer">
           <div class="footer-text">
             <p>Cypyright &copy;{{ year }} by Codehal | All Rights Reservied. </p>
           </div>
-          <div class="footer-iconTop">
+          <!-- <div class="footer-iconTop">
             <NuxtLink class="link-top" to=""><UIcon class="link-top-icon w-10 h-10 text-gray-900" name="i-heroicons-arrow-long-up-16-solid" /></NuxtLink>
-          </div>
+          </div> -->
         </footer>
   </div>
 </template>
