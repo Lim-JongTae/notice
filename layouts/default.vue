5f027790668598f04b1c6b48efd1304ff87d8670
JongTae <ljt0721@gmail.com>
2024-11-08 16:17:54 +0900

    feat:SeoMeta add

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,20 +7,20 @@
 
 <script setup>
 
-// useSeoMeta({
-//     title: '대부도 2024탐조대회',
-//     description: '2024년 HPAI및 생물다양성 교육',
-//     ogTitle: '2024년 HPAI & 생물다양성교육',
-//     ogDescription: '울산시민과학 도요새탐조단을 중심으로 생물다양성및 조류독감에 관학 교육',    
-//     ogImage: 'https://drive.google.com/uc?export=view&id=1hoWt9nZaAhcSl7RY26TrTJuiI0i3QEfY',
-//     ogUrl: 'https://drive.google.com/uc?export=view&id=1gEljICTymU-3Tlim8lRp-X9dlrI09pd6',
-//     ogLocale: 'ko_KR',
-//     ogSite_name: '울산시민과학단',
-//     twitterTitle: '울산시민과학단',
-//     twitterDescription: '울산시민과학단',
-//     twitterImage: '울산시민과학단',
-//     twitterCard: '울산시민과학단'
-// })
+useSeoMeta({
+    title: '2024 Bird Watching Race',
+    description: '2024년 대부도 탐조대회',
+    ogTitle: '2024년 대부도 탐조대회',
+    ogDescription: '이동성 물새들의 계절을 맞아 안산 대부도의 EAAF 자연생태를 체험하고 대부도 서식지의 이해와 탐조관광의 기회를 확대키 위함',    
+    ogImage: 'https://drive.google.com/uc?export=view&id=1e4qI-veCIqy9cVap-u9FEpLcEMVA4-sO',
+    ogUrl: 'https://drive.google.com/uc?export=view&id=1kq3SelRuE2TOS_LMDp1uEO2Aps68yPai',
+    ogLocale: 'ko_KR',
+    ogSite_name: '2024년 대부도 탐조대회',
+    twitterTitle: '2024년 대부도 탐조대회',
+    twitterDescription: '2024년 대부도 탐조대회',
+    twitterImage: '2024년 대부도 탐조대회',
+    twitterCard: '2024년 대부도 탐조대회'
+})
 
 </script>
 
